070a385dc203e0c482356b6485d692eb192c23bf
SXRIPT <Filip.Adamovic@studierende.htl-donaustadt.at>
2020-07-28 21:50:15 +0200

    works

--- alphabot/test/server.test.js
+++ alphabot/test/server.test.js
@@ -1,70 +1,34 @@
 
 // requiring the dev-dependencies
-var server = require('../src/server');
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let should = chai.should();
-let expect = chai.expect;
+let server = require('../src/server');
+let request = require('supertest');
 
-chai.use(chaiHttp);
 describe('/GET root path', () => {
-    beforeEach(function () {
-        server = require('../src/server');
-    });
-    afterEach(function () {
-        server.close();
-    });
     it('it should GET the root path', (done) => {
-        chai.request(server)
+        request(server)
            .get('/')
-           .end((end, res) => {
-               res.should.have.status(200);
-               done();
-           });
+            .expect(200, done);
     });
 });
 
 describe('/GET not existing path', () => {
-    beforeEach(function () {
-        server = require('../src/server');
-    });
-    afterEach(function () {
-        server.close();
-    });
    it('it should try GET non existing path', (done) => {
-       chai.request(server)
+       request(server)
           .get('/thisDoesntExists')
-          .end((err, res) => {
-             res.should.have.status(404);
-             done();
-          });
+          .expect(404, done)
    });
 });
 
 describe('/GET root path to check headers', () => {
-    beforeEach(function () {
-        server = require('../src/server');
-    });
-    afterEach(function () {
-        server.close();
-    });
    it('it should GET to the root path and have the helmet default headers set', (done) => {
-      chai.request(server)
+      request(server)
           .get('/')
-          .end((err, res) => {
-              expect(res).to.have.header('X-DNS-Prefetch-Control', 'off');
-              expect(res).to.have.header('X-Frame-Options', 'SAMEORIGIN');
-              expect(res).to.have.header('Strict-Transport-Security');
-              expect(res).to.have.header('X-Download-Options', 'noopen');
-              expect(res).to.have.header('X-Content-Type-Options', 'nosniff');
-              expect(res).to.have.header('X-XSS-Protection', '1; mode=block');
-              expect(res).to.have.header('X-RateLimit-Limit');
-              expect(res).to.have.header('X-RateLimit-Remaining');
-              done();
+          .expect('X-DNS-Prefetch-Control', 'off')
+          .expect('X-Frame-Options', 'SAMEORIGIN')
+          .expect('X-Download-Options', 'noopen')
+          .expect('X-Content-Type-Options', 'nosniff')
+          .expect('X-XSS-Protection', '1; mode=block')
+          .expect(200, done);
           });
-       afterEach( ( done ) => {
-           server.close(done());
-       });
-   });
 });
-
+server.close();
