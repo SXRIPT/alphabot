818163c5126d78d2a050c7fd691d130f2ee0cebb
SXRIPT <Filip.Adamovic@studierende.htl-donaustadt.at>
2020-07-28 20:24:32 +0200

    added unit testing

--- alphabot/src/server.js
+++ alphabot/src/server.js
@@ -5,14 +5,11 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const port = process.env.PORT || 8080;
 require('./alphabot');
+
 // rate limiter
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
     max: 50 // limit each IP to 50 requests per windowMs
-});
-
-app.get('/', (req,res) => {
-   res.status(200).send('ALPHABOT UI NEEDED')
 });
 
 // routes
@@ -26,6 +23,10 @@ app.use(helmet());
 app.use(volleyball);
 app.use(limiter); // all requests
 
+app.get('/', (req,res) => {
+    res.status(200).send('ALPHABOT UI NEEDED')
+});
+
 app.use('/session', session);
 app.use('/commands', commands);
 app.use('/chat', chat);
@@ -34,4 +35,8 @@ app.use((req, res) => {
     res.status(404).send('Unknown Request: ' + req.originalUrl);
 });
 
-app.listen(port, () => console.log('Server started on port: ' + port));
+
+
+/*let server = */ app.listen(port, () => console.log('Server started on port: ' + port));
+
+module.exports = app;
---
+++ alphabot/test/server.test.js
@@ -0,0 +1,48 @@
+
+// requiring the dev-dependencies
+let server = require('../src/server');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+let expect = chai.expect;
+
+chai.use(chaiHttp);
+describe('/GET root path', () => {
+    it('it should GET the root path', (done) => {
+        chai.request(server)
+           .get('/')
+           .end((end, res) => {
+               res.should.have.status(200);
+               done();
+           });
+    });
+});
+
+describe('/GET not existing path', () => {
+   it('it should try GET non existing path', (done) => {
+       chai.request(server)
+          .get('/thisDoesntExists')
+          .end((err, res) => {
+             res.should.have.status(404);
+             done();
+          });
+   });
+});
+
+describe('/GET root path to check headers', () => {
+   it('it should GET to the root path and have the helmet default headers set', (done) => {
+      chai.request(server)
+          .get('/')
+          .end((err, res) => {
+              expect(res).to.have.header('X-DNS-Prefetch-Control', 'off');
+              expect(res).to.have.header('X-Frame-Options', 'SAMEORIGIN');
+              expect(res).to.have.header('Strict-Transport-Security');
+              expect(res).to.have.header('X-Download-Options', 'noopen');
+              expect(res).to.have.header('X-Content-Type-Options', 'nosniff');
+              expect(res).to.have.header('X-XSS-Protection', '1; mode=block');
+              expect(res).to.have.header('X-RateLimit-Limit');
+              expect(res).to.have.header('X-RateLimit-Remaining');
+              done();
+          })
+   });
+});
