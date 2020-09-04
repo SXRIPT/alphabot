08d32fe018a168edb42ade20d4d4031b73d46b34
SXRIPT <Filip.Adamovic@studierende.htl-donaustadt.at>
2020-09-04 21:02:33 +0200

    fixed db command lookup bug & reverted morgan change
    - fixed db command lookup on every chat message
    - reverted morgan change

--- alphabot/helpers/chatTokenizer.js
+++ alphabot/helpers/chatTokenizer.js
@@ -1,6 +1,14 @@
 const checkCommand = require('./checkCommand');
 
 const tokenizer = async (channel, user, message) => {
+  /*
+  *  checks if the chat message start with one of the allowed
+  *  characters for a command later it will be checked if it
+  *  is the right character for the command.
+  */
+  const regex = /[`!@#$%^&_+\-=;:.<>?~]/;
+  if(!regex.test(message.charAt(0))) return;
+
   const token = [];
   channel = channel.replace('#', '');
   token.push(channel, user, message);
