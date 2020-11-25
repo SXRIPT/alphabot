7d0613c272cda7e4263b096a161c5a4cf4bd79de
SXRIPT <Filip.Adamovic@studierende.htl-donaustadt.at>
2020-11-25 21:44:27 +0100

    implement uptime variable

--- alphabot/twitch/index.js
+++ alphabot/twitch/index.js
@@ -2,10 +2,12 @@ const getViewers = require('./api/get-Streams/getViewers');
 const getGame = require('./api/get-Streams/getGame');
 const getLanguage = require('./api/get-Streams/getLanguage');
 const getTitle = require('./api/get-Streams/getTitle');
+const getUptime = require('./api/get-Streams/getUptime');
 
 module.exports = {
   getViewers,
   getGame,
   getLanguage,
   getTitle,
+  getUptime,
 }
